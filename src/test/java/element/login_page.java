5f5e3223f138e8916c7235fd1360f213367959ab
Gudla Sanjana Tejaswani <gudla.sanja@hcltech.com>
2025-08-25 12:49:44 +0530

    updated login page

---
+++ src/test/java/element/login_page.java
@@ -0,0 +1,37 @@
+package Elements;
+ 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+ 
+public class login_page {
+    WebDriver driver;
+ 
+    public login_page(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+    
+    @FindBy(name = "user-name")
+	public
+    WebElement userName;
+ 
+    @FindBy(name = "password")
+    WebElement password;
+ 
+    @FindBy(name = "login-button")
+    WebElement loginButton;
+ 
+    public void enterUserName(String username) {
+        userName.sendKeys(username);
+    }
+ 
+    public void enterPassword(String pwd) {
+        password.sendKeys(pwd);
+    }
+ 
+    public void clickLoginButton() {
+        loginButton.click();
+    }
+}
